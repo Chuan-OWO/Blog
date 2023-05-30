bcb0a519fc14a03eb6067131ebb1dc2aeb3d9bde
chuan <101337985+Chuan-OWO@users.noreply.github.com>
2023-05-30 13:31:07 +0800

    //創建新的User

--- router/api/auth.js
+++ router/api/auth.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const jwt = require('jsonwebtoken')
 const User = require('../../models/usersModel')
 
+
 //註冊 修改 驗證 登入 登出
 
 //用戶註冊
@@ -12,6 +13,7 @@ router.post('/register', async (req, res) => {
 
     try {
         //檢查輸入是否為空
+        
         if(!email || !username || !password){
             return res.status(400).json(
             { error: `email:${email}不能為空、username:${username}不能為空、password:${password}不能為空 ` });
@@ -35,14 +37,16 @@ router.post('/register', async (req, res) => {
 
             return res.status(400).json({ error: 'email 已經存在' });
         }
-
-        //創建新的user
-        const newUser = new User({ email,username,password });
-        await newUser.save();
-
+        
+        //創建新的User
+        // const User = new User({ email,username,password });
+        // const User = 
+        await User.create({ email,username,password });
+        
+        console.log("@@@@",User)
+        
         res.status(200).json({ message: '註冊成功' });
-
-
+        
     } catch (error) {
       res.status(500).json({ error: '註冊失敗' });
 

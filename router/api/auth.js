1cc2aa01e6b4fd17156f3af10ec1d6c364a34966
chuan <101337985+Chuan-OWO@users.noreply.github.com>
2023-05-30 11:59:17 +0800

    檢查接收註冊資料是否為空

--- router/api/auth.js
+++ router/api/auth.js
@@ -8,13 +8,37 @@ const User = require('../../models/usersModel')
 
 //用戶註冊
 router.post('/register', async (req, res) => {
-    try {
-      const { username, password } = req.body;
+    const { email,username,password } = req.body;
 
-      const user = new User({ username, password });
-      await user.save();
-      res.status(200).json({ message: '註冊成功' });
+    try {
+        //檢查輸入是否為空
+        if(!email || !username || !password){
+            return res.status(400).json(
+            { error: `email:${email}不能為空、username:${username}不能為空、password:${password}不能為空 ` });
+        }
+
+       
+
+        // 檢查用戶是否存在
+        const existingUser = await User.findOne({ email });
+            if (existingUser) {
+
+            return res.status(400).json({ error: 'email 已經存在' });
+        }
+
+        //創建新的user
+        const newUser = new User({ email,username,password });
+        await newUser.save();
+
+        res.status(200).json({ message: '註冊成功' });
+
     } catch (error) {
       res.status(500).json({ error: '註冊失敗' });
+
     }
   });
+
+
+
+
+module.exports = router

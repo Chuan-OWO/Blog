cdb8dabab26c6283b644d55517fa3675e3b997eb
chuan <101337985+Chuan-OWO@users.noreply.github.com>
2023-06-01 02:09:04 +0800

    // 與User 產生關聯

--- models/postModel.js
+++ models/postModel.js
@@ -5,10 +5,11 @@ const Schema = mongoose.Schema
 const postSchema = new Schema({
     title: String,
     content: String,
+    // 與User 產生關聯
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User'
     }
   });
 
-  module.exports = mongoose.model('post', postSchema);
+module.exports = mongoose.model('post', postSchema);
